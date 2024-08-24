b6b3f9b36db165ef572876b4a96d30fca8b5ae73
arum puri pratamawati <103489875+arumpuri@users.noreply.github.com>
2024-08-24 23:55:36 +0700

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -4,39 +4,48 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
-    userName: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    firstName: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    password: {
-      type: String,
-      trim: true,
-      unique: "Email already exists",
-      match: [/.+\@.+\..+/, "Please fill a valid email address"],
-      required: "Email is required",
-    },
-    role: {
-      type: String,
-      required: true,
-    },
+ name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['student', 'instructor', 'admin'],
+    default: 'student',
+  },
   },
   { timestamps: true }
 );
+
+
+// Hash password before saving the user
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to check password validity
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
